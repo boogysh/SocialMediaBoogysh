623c0b6395b417fb57fedd611b9cfe3a15d3854c
Victor Buga <bugavictor86@gmail.com>
2023-05-13 12:57:11 +0200

    myPost url

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -60,7 +60,7 @@ const MyPostWidget = () => {
 
       const response = await axios.post(
         // `http://localhost:3001/posts`,
-        `https://sm-boogysh-server-git-main-boogysh.vercel.app`,
+        `https://sm-boogysh-server-git-main-boogysh.vercel.app/posts`,
         formData,
         {
           headers: {
